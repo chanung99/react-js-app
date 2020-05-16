e8ecd1ce12808522a44e2befe7a699c669216cd9
chanung <chanung5722004@gmail.com>
2020-05-16 23:03:50 +0900

    Updates

---
+++ precache-manifest.df60f752ac94b4ad77a0b6275d504044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4f66a68f34a1b1d3c0734d977e4a01",
+    "url": "/react-js-app/index.html"
+  },
+  {
+    "revision": "3c312e1de9f5b8dd78a5",
+    "url": "/react-js-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22d5a6e2e836c44b31bd",
+    "url": "/react-js-app/static/js/2.e7b00c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-js-app/static/js/2.e7b00c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c312e1de9f5b8dd78a5",
+    "url": "/react-js-app/static/js/main.e5e23891.chunk.js"
+  },
+  {
+    "revision": "4e453289241c0f89a9d4",
+    "url": "/react-js-app/static/js/runtime-main.3d8192f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-js-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
